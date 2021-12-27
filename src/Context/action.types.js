22b64520a31fb029f59bba19436e013249ba908c
hrishikesh <hshinde166@gmail.com>
2021-12-27 23:31:48 +0530

    Created the actions that the project will perform

---
+++ src/Context/action.types.js
@@ -0,0 +1,4 @@
+//Here we create the functionality of our project like what are the tasks thata our project will perform
+
+export const ADD_TODO = "ADD_TODO";
+export const REMOVE_TODO = "REMOVE _TODO";
