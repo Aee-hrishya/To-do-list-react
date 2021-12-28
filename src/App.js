a7405b31e43980c3d2975ba0bd73d3af84dae9cc
hrishikesh <hshinde166@gmail.com>
2021-12-28 19:23:21 +0530

    Adding all the different components together

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TodoContext from "./Context/TodoContext";
 import TodoReducer from "./Context/reducer";
+import TodoForm from "./Components/Inputform";
+import Todos from "./Components/Todos";
 
 const App = () => {
 
@@ -11,9 +13,11 @@ const App = () => {
   const [todos, dispatch] = useReducer(TodoReducer, []);//This takes in two parameters the reducer function and the initial state and it returns and array of two elements one is the current state of the application and the other is the dispatch function through which we can update the state.
 
   return(
-    <TodoContext.Provider value={todos, dispatch}>
+    <TodoContext.Provider value={{todos, dispatch}}>
       <Container fluid>
         <h1>Todo list app with Context API</h1>
+        <Todos />
+        <TodoForm />
       </Container>
     </TodoContext.Provider>
   );
